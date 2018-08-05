baeb10982d0de52ae16f6fc1d30f3ec614a02fba
rusticpenguin <georgechios.music@gmail.com>
2018-08-04 22:15:09 -0600

    Created function assignKeyNotes: generates keys inside keyboard div. Iterates from a through g, repeating every time it hits the end of the array. User can choose as many keys as they want

--- keyboard.js
+++ keyboard.js
@@ -4,7 +4,22 @@ const keyboardSize = 12;
     const keyboard = document.querySelector("#keyboard");
     for(i = 0; i < keyboardSize; i++){
         const key = document.createElement("div");
-        key.classList.add("key")
-        keyboard.appendChild(key)
-    }
+        key.classList.add("key");
+        keyboard.appendChild(key);
+    };
+    assignKeyNotes();
 })()
+
+function assignKeyNotes(){
+    const notes = {
+        noteLetters: ["a", "b", "c", "d", "e", "f", "g"],
+        offset: 0
+    };
+    const keys = document.querySelectorAll(".key");
+    for(i = 0; i < keyboardSize; i++){
+        if (!(i === 0) && (i % 6 === 0)){
+            notes.offset += 6;
+        }
+        keys[i].classList.add(notes.noteLetters[i - notes.offset]);
+    }
+}
