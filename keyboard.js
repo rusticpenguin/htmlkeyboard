531ac1d3e9a2b936cb64c7d923cba8b4107eeffb
George Chios <hello@georgechios.com>
2023-02-15 20:12:09 -0700

    updates keyboard to properly get classname of key

--- keyboard.js
+++ keyboard.js
@@ -1,48 +1,74 @@
-const keyboardSize = 15;
+const keyboardSize = 25;
+const noteLetters = [
+  "a",
+  "aSharp",
+  "b",
+  "c",
+  "cSharp",
+  "d",
+  "dSharp",
+  "e",
+  "f",
+  "fSharp",
+  "g",
+  "gSharp",
+];
+const noteColors = [
+  "white",
+  "black",
+  "white",
+  "white",
+  "black",
+  "white",
+  "black",
+  "white",
+  "white",
+  "black",
+  "white",
+  "black",
+];
+
 const octave = 3;
 
-(function createKeyboard(){
-    const keyboard = document.querySelector("#keyboard");
-    generateKeys();
-    assignKeyNotes();
-})()
+function generateKeys() {
+  const keyboard = document.querySelector("#keyboard");
 
-function generateKeys(){
-    for(i = 0; i < keyboardSize; i++){
-        const key = document.createElement("div");
-        key.classList.add("key");
-        key.addEventListener("click", keyPressed, false);
-        keyboard.appendChild(key);
-    };
+  for (let i = 0; i < keyboardSize; i++) {
+    const key = document.createElement("div");
 
+    key.classList.add("key");
+    key.addEventListener("click", keyPressed, false);
+    keyboard.appendChild(key);
+  }
 }
 
-function assignKeyNotes(){
-    const notes = {
-        noteLetters: ["a", "aSharp", "b", "c", "cSharp", "d", "dSharp", "e", "f", "fSharp", "g", "gSharp"],
-        noteColors: ["white", "black", "white", "white", "black", "white", "black", "white", "white", "black", "white", "black"],
-        offset: 0
-    };
-    const keys = document.querySelectorAll(".key");
-    for(i = 0; i < keyboardSize; i++){
-        if (!(i === 0) && (i % 12 === 0)){
-            notes.offset += 12;
-        }
-        keys[i].classList.add(notes.noteLetters[i - notes.offset] + (octave + (notes.offset/12)));
-        keys[i].classList.add(notes.noteColors[i - notes.offset]);
-    };
+function assignKeyNotes() {
+  let offset = 0;
+  const keys = document.querySelectorAll(".key");
+  for (let i = 0; i < keyboardSize; i++) {
+    if (i !== 0 && i % 12 === 0) {
+      offset += 12;
+    }
+
+    keys[i].classList.add(noteLetters[i - offset] + (octave + offset / 12));
+    keys[i].classList.add(noteColors[i - offset]);
+  }
 }
 
-function keyPressed(target){
-    notePressed = target.path[0].classList[1];
-    getHertz(notePressed);
-    console.log(hertz);
+function keyPressed(event) {
+  notePressed = event.target.classList[1];
+  console.log(getHertz(notePressed));
 }
 
-function getHertz(note){
-    return hertz = hertzMath(note);
+function getHertz(note) {
+  return hertzMath(note);
 }
 
-function hertzMath(note){
-    return note;
+function hertzMath(note) {
+  return note;
 }
+
+(function createKeyboard() {
+  generateKeys();
+  assignKeyNotes();
+})();
